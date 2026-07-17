d2a8b09b2e213d103a651aed93cda30b81d73c76
Artem <artemyarin15@gmail.com>
2026-07-17 16:57:02 +0200

    feat: implement HashPassword and CheckPassword for user authentication

---
+++ internal/app/auth/password/password.go
@@ -0,0 +1,16 @@
+package password
+
+import "golang.org/x/crypto/bcrypt"
+
+func HashPassword(password string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	if err != nil {
+		return "", err
+	}
+	return string(hash), nil
+}
+
+func CheckPassword(hashedPassword, password string) error {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+	return err
+}
