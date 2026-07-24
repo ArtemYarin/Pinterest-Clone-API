2e5b677a462e73dfb758c258f186401a8cffeb11
Artem <artemyarin15@gmail.com>
2026-07-24 11:53:41 +0200

    feat: add pins migrations

---
+++ internal/app/pin/migrations/000001_create_pins_table.up.sql
@@ -0,0 +1,14 @@
+CREATE TABLE pins (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID NOT NULL, -- References User Service, but NO FK constraint
+    title VARCHAR(255) NOT NULL,
+    image_url VARCHAR(255) NOT NULL UNIQUE,
+    description VARCHAR(1000),
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    likes_count INTEGER DEFAULT 0
+);
+
+CREATE INDEX idx_pins_title ON pins(title);
+CREATE INDEX idx_pins_user_id ON pins(user_id);
+CREATE INDEX idx_pins_created_at ON pins(created_at DESC);
