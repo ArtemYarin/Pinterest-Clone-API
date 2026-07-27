1911baf0e515c8cc8467c31e486054b4f5a9b1bf
Artem <artemyarin15@gmail.com>
2026-07-27 14:55:07 +0200

    feat: wire routers

--- internal/router/router.go
+++ internal/router/router.go
@@ -2,13 +2,16 @@ package router
 
 import (
 	"github.com/ArtemYarin/pinterest-clone-api/internal/app/auth"
+	"github.com/ArtemYarin/pinterest-clone-api/internal/app/pin"
 	"github.com/go-chi/chi/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-func SetupRouter(userHandler auth.UserHandler) chi.Router {
+func SetupRouter(userHandler auth.UserHandler, pinHandler pin.PinHandler, authPool *pgxpool.Pool, pinPool *pgxpool.Pool) chi.Router {
 	r := chi.NewRouter()
 
-	r.Mount("/auth", auth.UserRouter(&userHandler))
+	r.Mount("/auth", auth.UserRouter(&userHandler, authPool))
+	r.Mount("/pins", pin.PinRouter(&pinHandler, pinPool))
 
 	return r
 }
